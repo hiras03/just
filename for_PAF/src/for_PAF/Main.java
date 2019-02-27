3ccfe6a8ee3065562fdbb1b9d78d3a092e6b596d
hiras03 <hirasharis03@gmail.com>
2019-02-26 19:22:06 -0800

    cut it and put ZeRo

---
+++ for_PAF/src/for_PAF/Main.java
@@ -0,0 +1,12 @@
+package for_PAF;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Cut it and Put ZeRo!!");
+
+	}
+
+}
